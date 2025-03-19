7980a8e74beee1d523e17c6185b274dd2df602c1
Maliha Tanweer <malihatanweer@gmail.com>
2025-03-19 12:06:46 -0400

    Updates to cor

--- index.js
+++ index.js
@@ -18,20 +18,23 @@ app.use('/uploads', express.static('uploads'));
 const PORT = process.env.PORT || 7000
 
 // app.use(cors()); // Allows requests from different origins
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://financetrackerfrontend.vercel.app/");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://financetrackerfrontend.vercel.app/");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+// });
 app.use("/uploads", express.static("uploads"));
 app.use(express.json()); // Middleware to parse JSON requests
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
+
 
 // Define a basic route
 
-app.use('/', cors(), financialRecordRouter);
-app.use('/api', cors(), newTotalRouter);
+app.use('/', financialRecordRouter);
+app.use('/api', newTotalRouter);
 
 // Connect to Mongodb
 mongoose.connect(process.env.MONGO_URI, {
